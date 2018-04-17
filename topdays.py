7d7b8e35cc00e6f3372f0278fdf4fa6bc4f7a44a
Mark Smith <smith.h.mark@gmail.com>
2018-04-17 15:11:53 -0700

    reporting low and high peaks

--- topdays.py
+++ topdays.py
@@ -5,17 +5,21 @@ import events
 
 def main(path, n):
 
-    c, r = events.makeHighestDaysMonitorReporter(n)
+    maxNc, maxNr = events.makeHighestDaysMonitorReporter(n)
+    minNc, minNr = events.makeLowestDaysMonitorReporter(n)
     with open(path, newline='') as ifil:
         #recs = util.recordsFromStream(ifil)
         recs = csv.DictReader(ifil)
         evs = events.to_event_stream(recs)
         popEvents = events.tallyPopulation(evs)
-        events.consumePEvent(popEvents, c)
+        events.consumePEvent(popEvents, maxNc, minNc)
 
     print("Top {} peak days".format(n))
     print("Usage Peak\tDate")
-    for pop, day in r():
+    for pop, day in maxNr():
+        print("\t{}\t{}".format(pop, day))
+    print("\nLow Peak\tDate")
+    for pop, day in minNr():
         print("\t{}\t{}".format(pop, day))
 
 

73656224a87717d25ba1750ef18b371a4449197d
Mark Smith <smith.h.mark@gmail.com>
2018-04-19 09:08:45 -0700

    partial refactor to fold window

--- events.py
+++ events.py
@@ -53,45 +53,65 @@ def makeCounter():
         return count
     return consumer, reporter
 
-def makeHighestDaysMonitorReporter(n):
+def nlargestPerWindow(n, extract_val, base_case, accum, windowid):
     hp = []
-    prevday = None
-    curmax = 0
+    prevwindow = None
+    window_acc = base_case 
     def consumer(event):
-        nonlocal prevday
-        nonlocal curmax
+        nonlocal prevwindow
+        nonlocal window_acc
         nonlocal hp
-        epop = event[-1]['pop']
-        #curday = event[1].date()
-        curday = eventdate(event)
-        if prevday is None:
-            prevday = curday
-        if curday != prevday:
-            # deal with storing day's day
-            newelt = (curmax, prevday)
-            if len(hp) == n and hp[0][0] < curmax:
+        window = windowid(event)
+        curval = extract_val(event)
+        if prevwindow is None:
+            prevwindow = window
+        if window != prevwindow:
+            newelt = (window_acc, prevwindow)
+            if len(hp) == n and hp[0][0] < window_acc:
                 heapq.heapreplace(hp, newelt)
             elif len(hp) < n:
                 heapq.heappush(hp, newelt)
-            prevday = curday
-            curmax = 0
-        else:
-            if epop > curmax:
-                curmax = epop
+            prevwindow = window
+            window_acc = base_case
+        window_acc = accum(curval, window_acc)
     def reporter():
-        nonlocal prevday
-        nonlocal curmax
+        nonlocal prevwindow
+        nonlocal window_acc
         nonlocal hp
         # deal with storing day's day
-        if len(hp) == n and hp[0][0] < curmax:
-            heapq.heapreplace(hp, (curmax, prevday))
+        if len(hp) == n and hp[0][0] < window_acc:
+            heapq.heapreplace(hp, (window_acc, prevwindow))
         elif len(hp) < n:
-            heapq.heappush(hp, (curmax, prevday))
+            heapq.heappush(hp, (window_acc, prevwindow))
         tmp = [heapq.heappop(hp) for ii in range(len(hp))]
         return tmp
     return consumer, reporter
 
+def makeHighestDaysMonitorReporter(n):
+    def accum(cur, existing):
+        if cur > existing:
+            return cur
+        return existing
+    def extract_val(event):
+        return event[-1]['pop']
+    base_case = 0
+    def windowid(event):
+        return eventdate(event)
+    return nlargestPerWindow(n, extract_val, base_case, accum, windowid)
+
 def makeLowestDaysMonitorReporter(n):
+    '''
+    def accum(cur, existing):
+        if cur > existing:
+            return cur
+        return existing
+    def extract_val(event):
+        return -1 * event[-1]['pop']
+    base_case = 0
+    def windowid(event):
+        return eventdate(event)
+    return nlargestPerWindow(n, extract_val, base_case, accum, windowid)
+    '''
     hp = []
     prevday = None
     curmax = 0
